b9ab41af6e250936c6c7a3c20d3d0b2f28722120
Stanislav Termosa <termosa.stanislav@gmail.com>
2017-02-04 16:35:36 +0200

    Add alias support

--- bin/go.js
+++ bin/go.js
@@ -2,7 +2,21 @@
 var path = require('path');
 var shell = require('shelljs');
 var download = require('download-github-repo');
+var fs = require('fs');
 require('../lib/which');
+
+const storePath = path.resolve(path.join(__dirname, '..', 'store.json'));
+
+if (!fs.existsSync(storePath)) {
+  fs.writeFileSync(storePath, JSON.stringify({}));
+}
+
+var aliases = require(storePath);
+
+const goWhiteList = [
+  /^[-a-z\d]+\/[-_a-z\d]+$/i,
+  /^alias$/i
+];
 
 var args = process.argv.slice(2);
 
@@ -13,15 +27,27 @@ if (!args.length) {
   console.log('Specify repositiory');
   process.exit(1);
 }
-const dir = args.length > 1 ? args[1] : args[0].split('/')[1];
-go(dir, args[0]);
+
+if (args[0] == 'alias') {
+  if (args.length < 3) {
+    console.log('provide source and name for alias');
+    process.exit(4);
+  }
+  aliases[args[2]] = args[1];
+  fs.writeFileSync(storePath, JSON.stringify(aliases));
+} else {
+  let dir = args.length > 1 ? args[1] : args[0].split('/')[1];
+  if (!dir) dir = args[0];
+  let repo = aliases[args[0]] || args[0];
+  go(dir, repo);
+}
 
 function runGo(whichGo) {
   var bins = whichGo.split('\n')
     .filter(function(bin) { return bin });
 
-  if (bins.length > 1) {
-    if (args.length && !args[0].match(/^[-a-z\d]+\/[-_a-z\d]+$/i)) {
+  if (bins.length > 1 && args.length) {
+    if (matchGoCommand(args)) {
       shell.exec([bins[1]].concat(args).join(' '));
       return true;
     }
@@ -30,11 +56,19 @@ function runGo(whichGo) {
   return false;
 }
 
+function matchGoCommand(args) {
+  var isAlias = !!aliases[args[0]];
+  if (isAlias) return false;
+  return !goWhiteList.find(rule => !args[0].match(rule));
+}
+
 function go(dest, repo) {
-  var res = shell.mkdir(path.resolve(dest));
-  if (shell.error()) {
-    console.log('Can not create directory (' + dest + '):', res.stderr);
-    process.exit(2);
+  if (!fs.existsSync(path.resolve(dest))) {
+    let res = shell.mkdir(path.resolve(dest));
+    if (shell.error()) {
+      console.log('Can not create directory (' + dest + '):', res.stderr);
+      process.exit(2);
+    }
   }
 
   console.log('Loading sources...');
