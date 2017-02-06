b16f654369240cae7a6f4617747c74fab1f493dc
Stanislav Termosa <termosa.stanislav@gmail.com>
2017-02-06 23:12:50 +0200

    Add ESlint confif

--- commands/help.js
+++ commands/help.js
@@ -2,5 +2,5 @@ var complete = require('../lib/complete');
 
 //$ go
 module.exports = function() {
-  console.log('Keep calm. Read docs https://github.com/termosa/go');
+  complete('Keep calm. Read docs https://github.com/termosa/go');
 };
