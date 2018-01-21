19d59755f551ff9a3f62632cd132c64c8461891c
Stanislav Termosa <termosa.stanislav@gmail.com>
2018-01-21 10:59:17 +0200

    Prevent registering same plugins

--- go.js
+++ go.js
@@ -1,6 +1,28 @@
 function use (plugin, options) {
-  plugin.install(this, options)
-  return this
+  if (this instanceof Go) {
+    if (matchPlugin(plugin, this)) return this
+
+    if (plugin && typeof plugin.install === 'function') {
+      plugin.install(this, options)
+    } else if (typeof plugin === 'function') {
+      plugin.call(null, this, options)
+    } else {
+      throw new ReferenceError('\'plugin\' must be a function or an object with install method')
+    }
+
+    this._plugins.push(plugin)
+
+    return this
+  } else {
+    throw new ReferenceError('use() should be called on instance of Go')
+  }
+}
+
+function matchPlugin (plugin, instance) {
+  for (var i = instance._plugins.length; i--;) {
+    if (instance._plugins[i] === plugin) return true
+  }
+  return false
 }
 
 var shouldCreateRealInstance = false
@@ -12,6 +34,7 @@ function Go () {
     shouldCreateRealInstance = false
     return go
   }
+  this._plugins = []
 }
 
 Go.prototype.use = use
