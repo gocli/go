f71d033c017a0c734003ca5293d312f6765277c7
Stanislav Termosa <termosa.stanislav@gmail.com>
2018-01-26 23:19:21 +0200

    Use go-plugin-handlebars

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var go = require('./go')()
 
 var FSPlugin = require('go-plugin-fs')
 var QuizPlugin = require('./plugins/quiz')
-var HandlebarsPlugin = require('./plugins/handlebars')
+var HandlebarsPlugin = require('go-plugin-handlebars')
 
 module.exports = go
   .use(FSPlugin)
