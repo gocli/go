3b2ac19da60cbee9c31a1de6b311f531aee59aa2
Stanislav Termosa <termosa.stanislav@gmail.com>
2018-01-31 14:48:20 +0200

    Add go-plugin-cli

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var go = require('./go')()
 
+var CLIPlugin = require('go-plugin-cli')
 var FSPlugin = require('go-plugin-fs')
 var QuizPlugin = require('go-plugin-quiz')
 var HandlebarsPlugin = require('go-plugin-handlebars')
 
 module.exports = go
+  .use(CLIPlugin)
   .use(FSPlugin)
   .use(QuizPlugin)
   .use(HandlebarsPlugin)
